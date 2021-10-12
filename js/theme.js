0acf39c25f0d18a3d1610fb90ceeb3e763acf128
IvanaaVasic <ivana.p.vasic@gmail.com>
2021-10-12 22:32:09 +0200

    small changes

--- js/theme.js
+++ js/theme.js
@@ -12,13 +12,3 @@ input.addEventListener("change", function () {
   }
   console.log(input.value);
 });
-
-// let i = document.querySelector('input'),
-//     o = document.querySelector('output');
-
-// o.innerHTML = i.value;
-
-// // use 'change' instead to see the difference in response
-// i.addEventListener('input', function () {
-//   o.innerHTML = i.value;
-// }, false);
